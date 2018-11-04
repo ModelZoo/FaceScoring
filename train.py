c024613b56645199f4f333efb0ab9db8d23782dc
Germey <cqc@cuiqingcai.com>
2018-11-04 21:10:03 +0800

    update split

--- train.py
+++ train.py
@@ -25,18 +25,26 @@ class Trainer(BaseTrainer):
         zoom_range=0.1
     )
     
-    def enhance_image(self, image):
+    def enhance_images(self, x_data, y_data):
         """
         generate enhanced image
         :param image:
         :return:
         """
-        image = np.expand_dims(image, axis=0)
-        gen = self.image_generator.flow(image)
-        for i in range(self.flags.enhance_images_number):
-            enhanced_image = next(gen)
-            enhanced_image = np.reshape(enhanced_image, enhanced_image.shape[1:])
-            yield enhanced_image
+        x_data_enhanced, y_data_enhanced = [], []
+        for x, y in zip(x_data, y_data):
+            # add original data
+            x_data_enhanced.append(x)
+            y_data_enhanced.append(y)
+            # add enhanced data
+            image = np.expand_dims(x, axis=0)
+            gen = self.image_generator.flow(image)
+            for i in range(self.flags.enhance_images_number):
+                enhanced_image = next(gen)
+                enhanced_image = np.reshape(enhanced_image, enhanced_image.shape[1:])
+                x_data_enhanced.append(enhanced_image)
+                y_data_enhanced.append(y)
+        return np.asarray(x_data_enhanced), np.asarray(y_data_enhanced)
     
     def prepare_data(self):
         """
@@ -46,19 +54,13 @@ class Trainer(BaseTrainer):
         # read data
         x_data, y_data = [], []
         path_data = self.flags.data_dir
-        for index, image in enumerate(listdir(path_data)):
-            if index % 20 == 0:
-                print('Processing', index + 1, 'Images')
+        for image in listdir(path_data):
             image_path = join(path_data, image)
             label = int(image.split('-')[0]) - 1
             image_data = Image.open(image_path)
             image_array = np.reshape(np.asarray(image_data, dtype='float32'), [128, 128, 3])
             x_data.append(image_array)
             y_data.append(label)
-            # get enhanced images
-            for image in self.enhance_image(image_array):
-                x_data.append(image)
-                y_data.append(label)
         
         # to numpy
         x_data, y_data = np.asarray(x_data, dtype=np.float32), np.asarray(y_data, dtype=np.float32)
@@ -68,7 +70,11 @@ class Trainer(BaseTrainer):
         print('Len X', len(x_data), 'Len Y', len(y_data))
         # split data
         x_train, x_eval, y_train, y_eval = train_test_split(x_data, y_data, test_size=0.15)
-        print('Len X Train', len(x_train), 'Len X Eval', len(x_eval))
+        
+        (x_train, y_train), (x_eval, y_eval) = self.enhance_images(x_train, y_train), \
+                                               self.enhance_images(x_eval, y_eval)
+        
+        print('Len X Train', len(x_train), x_train.shape, 'Len X Eval', len(x_eval))
         print('Len Y Train', len(y_train), 'Len Y Eval', len(y_eval))
         return (x_train, y_train), (x_eval, y_eval)
 
